b9a0d74bcc0a51803d28ff2930df9092342f6380
Isaac DeBord <156871124+idebord@users.noreply.github.com>
2024-01-24 17:40:22 -0500

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Isaac DeBord>
+ *        Date: <01-24-2024>
+ * Description: Lab 2 "Hello Git" program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
